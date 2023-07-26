f4a757f0153088f8422cf490e9552d07b4716d65
RogelioMdz <rogelio.nava@live.com>
2023-07-26 17:16:50 -0600

    Numero romado a entero, Issue: falta mejorar rendimiento dio 5.9%

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -15,4 +15,39 @@ public class Solution {
         }
         return nums;
     }
+
+    public int romanToInt(String s){
+        HashMap<String, Integer> roman = new HashMap<>();
+        roman.put("I", 1);
+        roman.put("V", 5);
+        roman.put("X", 10);
+        roman.put("L", 50);
+        roman.put("C", 100);
+        roman.put("D", 500);
+        roman.put("M", 1000);
+
+        int valor = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            String currentSymbol = Character.toString(currentChar);
+
+            int currentValue = roman.getOrDefault(currentSymbol, 0);
+
+            if (i + 1 < s.length()) {
+                char nextChar = s.charAt(i + 1);
+                String nextSymbol = Character.toString(nextChar);
+                int nextValue = roman.getOrDefault(nextSymbol, 0);
+
+                if (currentValue < nextValue) {
+                    valor -= currentValue;
+                } else {
+                    valor += currentValue;
+                }
+            } else {
+                valor += currentValue;
+            }
+        }
+        System.out.println(valor);
+        return valor;
+    }
 }
